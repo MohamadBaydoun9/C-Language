96d4c11dce40fcae75db3647e0f4f96122c5265d
M0cker9 <ahsaans12344@gmail.com>
2022-06-30 11:02:30 +0300

    second Exercise

---
+++ Exam-2022-1-EX2.cpp
@@ -0,0 +1,19 @@
+		/*Write a Program That Calculates The Sum Of The Following Sequence 1,2,4,7,11,16,22...N
+		==>N is Given by Keyboard
+		if N=5 Then 1+2+4+7+11 diplays 25
+		*/
+#include<stdio.h>
+int main(){
+	int sum=0,nb=1,N,i;
+	
+	do{
+		scanf("%d",&N);
+	}while(N<0);
+
+	for(i=1;i<=N;i++){ 
+	 	sum = sum + nb; 
+	 	nb+=i; 
+ 	} 
+ 	
+	printf("\n%d",sum);
+}
